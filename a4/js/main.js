5824c055ce59b8419fd40d0a264b83f8e05c8ff9
Neil Sengupta <neil96sengupta@gmail.com>
2016-01-28 23:19:11 -0800

    burger

---
+++ a4/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  var count = 5;
+add_task(); 
+delete_task(); 
+function add_task() {
+
+$('.add-new-task').submit(function(){
+  var new_task = $('.add-new-task input[name=new-task]').val();
+  count = count + 1;
+
+  var print = '<li><span>' + new_task + '</span><img id="' + count + '" class="delete-button" width="10px" src="http://demo.mlitzinger.com/2014/tutorials/todo-list/images/close.svg" /></li>';
+  
+  if(new_task !== ''){
+    $('.add-new-task input[name=new-task]').val('');
+    $(print).appendTo('.task-list ul').hide().fadeIn();
+    delete_task();
+  }
+  return false;
+  });
+}
+
+function delete_task() {
+  $('.delete-button').click(function(){
+    var current_element = $(this);
+    var id = $(this).attr('id');
+    current_element.parent().fadeOut("fast", function() { $(this).remove(); });
+  });
+}
+})
