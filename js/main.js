fdbcd34fe2fc821d99dd60eee3c10306a116e644
Neil Sengupta <neil96sengupta@gmail.com>
2016-12-13 11:58:13 +0400

    Ported everything to Angular

--- js/main.js
+++ js/main.js
@@ -1,5 +1,62 @@
-$(document).ready(function(){
-	$("#header").hide().fadeIn(1500);
-	$("#projects").hide().fadeIn(1500);
-	$("#footnote").hide().fadeIn(1500);
-});
+(function(){
+	var app = angular.module("portfolio",[]);
+
+	app.controller("portfolioController", function(){
+		this.projects = listOfProjects;
+	});
+
+	app.directive("portfolio", function(){
+		return{
+			restrict: 'E', 
+			templateUrl: "/static/portfolio.html"
+		};
+	});
+
+	app.directive("introduction", function(){
+		return{
+			restrict: 'E',
+			templateUrl: "/static/introduction.html"
+		};
+	});
+
+	app.directive("projects", function(){
+		return{
+			restrict: 'E',
+			templateUrl: "/static/projects.html"
+		};
+	});
+
+	var listOfProjects = [
+		{
+			name:"Friends and Food",
+			skills:["Node JS", "Mongoose", "Express JS", "Sketch", "Invision"],
+			description: "Web application that pairs you with friends in your network to get food with based on your personalised schedule."
+		},
+		{
+			name:"Taskr",
+			skills:["Node JS", "Mongoose", "Express JS", "Sketch", "Invision"],
+			description: "Android Application for task allocation and management amongst friends living in shared households."
+		},
+		{
+			name:"Phetch",
+			skills:["Node JS", "Mongoose", "Express JS", "Sketch", "Invision"],
+			description: "iOS Application that helps tracking commodities, tasks and expenses in shared households."
+		},
+		{
+			name:"Data Science",
+			skills:["Python", "Numpy"],
+			description: "Maximum Likelihood Estimation on Unigram and Bigram Models of Data on English Text Dataset"
+		},
+		{
+			name:"Analyzing Yelp Reviews",
+			skills:["Python", "Numpy"],
+			description: "Maximum Likelihood Estimation on Unigram and Bigram Models of Data on English Text Dataset"
+		},
+		{
+			name:"Team based matchup analysis for DotA 2",
+			skills:["Python", "Numpy"],
+			description: "Maximum Likelihood Estimation on Unigram and Bigram Models of Data on English Text Dataset"
+		}	
+	];
+
+})();
